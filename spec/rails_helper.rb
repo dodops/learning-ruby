de4514e20238ecbf00185c05b1347afa7e2aebd1
Douglas Macedo <dodop3000@gmail.com>
2016-04-25 11:07:46 -0300

    set capybara timeout

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ require 'devise'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+Capybara.default_max_wait_time = 25
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
