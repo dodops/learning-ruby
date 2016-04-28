8b1adf5222f79d949fa49752897ecca035bed429
Daniel Moreto <dmoreto@ieintercambio.com.br>
2016-04-28 18:47:56 -0300

    Refactors the request headers

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
-describe Api::V1::UsersController do
-  before(:each) do
-    request.headers['Accept'] = "application/vnd.brasilsemzika.v1, #{Mime::JSON}"
-    request.headers['Content-Type'] = Mime::JSON.to_s
-  end
-
+describe Api::V1::UsersController, api: true do
   describe "GET #show" do
     before :each do
       @user = create(:user)
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -4,8 +4,30 @@ module Request
       @json_response ||= JSON.parse(response.body, symbolize_names: true)
     end
   end
+
+  module HeadersHelpers
+    def api_header(version = 1)
+      request.headers['Accept'] = "application/vnd.brasilsemzika.v#{version}"
+    end
+
+    def api_response_format(format = Mime::JSON)
+      request.headers['Accept'] += ",#{format}"
+      request.headers['Content-Type'] = format.to_s
+    end
+
+    def include_default_accept_headers
+      api_header
+      api_response_format
+    end
+  end
 end
+
 
 RSpec.configure do |config|
   config.include Request::JsonHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+  
+  config.before(:each, type: :controller, api: true) do
+    include_default_accept_headers
+  end
 end
