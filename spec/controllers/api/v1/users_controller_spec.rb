870045360e96eaf9e3df49d7c64d6896743277e7
Daniel Moreto <dmoreto@ieintercambio.com.br>
2016-04-28 18:31:51 -0300

    Adds JSON Content-Type as default on headers

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.brasilsemzika.v1" }
+  before(:each) do
+    request.headers['Accept'] = "application/vnd.brasilsemzika.v1, #{Mime::JSON}"
+    request.headers['Content-Type'] = Mime::JSON.to_s
+  end
 
   describe "GET #show" do
     before :each do
       @user = create(:user)
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
@@ -21,7 +24,7 @@ describe Api::V1::UsersController do
     context "when is successfully created" do
       before(:each) do
         @user_attributes = attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
@@ -35,7 +38,7 @@ describe Api::V1::UsersController do
     context "when is not created" do
       before(:each) do
         @invalid_user_attributes = attributes_for(:user, email: "")
-        post :create, { user: @invalid_user_attributes }, format: :json
+        post :create, { user: @invalid_user_attributes }
       end
 
       it "renders an errors json" do
@@ -56,7 +59,7 @@ describe Api::V1::UsersController do
     context "when is successfully updated" do
       before(:each) do
         @user = create :user
-        patch :update, { id: @user.id, user: { email: "newemail@example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "newemail@example.com" } }
       end
 
       it "renders the json representation for the updated user" do
@@ -70,7 +73,7 @@ describe Api::V1::UsersController do
     context "when is not created" do
       before(:each) do
         @user = create :user
-        patch :update, { id: @user.id, user: { email: "bademail.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "bademail.com" } }
       end
 
       it "renders an errors json" do
@@ -90,7 +93,7 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = create :user
-      delete :destroy, id: @user.id, format: :json
+      delete :destroy, id: @user.id
     end
 
     it { should respond_with 204 }
