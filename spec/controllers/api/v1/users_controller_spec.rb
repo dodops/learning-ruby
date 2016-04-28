6b776d3291889ebd5769c1dddd890d87f6393904
Daniel Moreto <dmoreto@ieintercambio.com.br>
2016-04-28 17:52:53 -0300

    Adds the user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,39 @@ describe Api::V1::UsersController do
 
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @invalid_user_attributes = attributes_for(:user, email: "")
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "não pode ficar em branco"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
